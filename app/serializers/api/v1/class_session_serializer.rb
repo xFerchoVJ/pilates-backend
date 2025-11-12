a4b2cf8c9075ed452cda3c066a245620b3b6c1d3
xFerchoVJ <fer_elsword@hotmail.com>
2025-11-11 18:06:20 -0600

    Add users_for_class attribute to ClassSessionSerializer to include associated users in the serialized output.

--- app/serializers/api/v1/class_session_serializer.rb
+++ app/serializers/api/v1/class_session_serializer.rb
@@ -1,8 +1,9 @@
 class Api::V1::ClassSessionSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :start_time, :end_time, :spots_left
+  attributes :id, :name, :description, :start_time, :end_time, :spots_left, :users_for_class
   has_one :instructor, serializer: Api::V1::UsersSerializer
   belongs_to :lounge, serializer: Api::V1::LoungesSerializer
   has_many :class_spaces
+
   def start_time
     object.start_time&.in_time_zone("America/Mexico_City")&.iso8601
   end
@@ -13,5 +14,9 @@ class Api::V1::ClassSessionSerializer < ActiveModel::Serializer
 
   def spots_left
     object.spots_left
+  end
+
+  def users_for_class
+    object.reservations.includes(:user).map { |reservation| Api::V1::UsersSerializer.new(reservation.user) }
   end
 end
