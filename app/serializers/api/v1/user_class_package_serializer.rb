528a69b7b847698d413b1aeafaaf0bce7f2e6e43
xFerchoVJ <fer_elsword@hotmail.com>
2025-11-20 23:53:30 -0600

    Enhance UserClassPackageSerializer to include 'expires_at' attribute for improved package expiration tracking.

--- app/serializers/api/v1/user_class_package_serializer.rb
+++ app/serializers/api/v1/user_class_package_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::UserClassPackageSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :class_package_id, :remaining_classes, :status, :purchased_at
+  attributes :id, :user_id, :class_package_id, :remaining_classes, :status, :purchased_at, :expires_at
   belongs_to :user, serializer: Api::V1::UsersSerializer
   belongs_to :class_package, serializer: Api::V1::ClassPackageSerializer
 end
